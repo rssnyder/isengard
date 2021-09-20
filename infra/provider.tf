b4ba0e54aa6b955426d9563fe32c92bade9604f7
Riley Snyder <rileysndr@gmail.com>
2021-09-20 16:09:16 -0500

    adding do infra tf

---
+++ infra/provider.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "digitalocean" {
+  token = "5b02f75508805bc32565c78a0e5a6ba775b6138dc47bcd5b664ea74cc68a67b4"
+}
