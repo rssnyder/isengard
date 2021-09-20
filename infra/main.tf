0bc19bb59efa90ffcdc1aa3425480484418f44a8
Riley Snyder <rileysndr@gmail.com>
2021-09-20 16:31:18 -0500

    tf fmt

--- infra/main.tf
+++ infra/main.tf
@@ -1,9 +1,9 @@
 resource "digitalocean_droplet" "do0" {
   name = "do0"
 
-  image = "ubuntu-20-04-x64"
-  region = "nyc3"
-  size = "s-1vcpu-1gb"
+  image      = "ubuntu-20-04-x64"
+  region     = "nyc3"
+  size       = "s-1vcpu-1gb"
   monitoring = true
   ssh_keys = [
     "98:9f:0d:4f:10:dc:61:a5:c5:14:f1:5a:88:cf:c1:76",
@@ -13,10 +13,10 @@ resource "digitalocean_droplet" "do0" {
 
 resource "digitalocean_droplet" "do1" {
   name = "do1"
-  
-  image = "ubuntu-20-04-x64"
-  region = "nyc3"
-  size = "s-1vcpu-1gb"
+
+  image      = "ubuntu-20-04-x64"
+  region     = "nyc3"
+  size       = "s-1vcpu-1gb"
   monitoring = true
   ssh_keys = [
     "98:9f:0d:4f:10:dc:61:a5:c5:14:f1:5a:88:cf:c1:76",
@@ -26,10 +26,10 @@ resource "digitalocean_droplet" "do1" {
 
 resource "digitalocean_droplet" "do2" {
   name = "do2"
-  
-  image = "ubuntu-20-04-x64"
-  region = "nyc3"
-  size = "s-1vcpu-1gb"
+
+  image      = "ubuntu-20-04-x64"
+  region     = "nyc3"
+  size       = "s-1vcpu-1gb"
   monitoring = true
   ssh_keys = [
     "98:9f:0d:4f:10:dc:61:a5:c5:14:f1:5a:88:cf:c1:76",
@@ -39,10 +39,10 @@ resource "digitalocean_droplet" "do2" {
 
 resource "digitalocean_droplet" "do3" {
   name = "do3"
-  
-  image = "ubuntu-20-04-x64"
-  region = "nyc3"
-  size = "s-1vcpu-1gb"
+
+  image      = "ubuntu-20-04-x64"
+  region     = "nyc3"
+  size       = "s-1vcpu-1gb"
   monitoring = true
   ssh_keys = [
     "98:9f:0d:4f:10:dc:61:a5:c5:14:f1:5a:88:cf:c1:76",
@@ -52,10 +52,10 @@ resource "digitalocean_droplet" "do3" {
 
 resource "digitalocean_droplet" "do4" {
   name = "do4"
-  
-  image = "ubuntu-20-04-x64"
-  region = "nyc3"
-  size = "s-1vcpu-1gb"
+
+  image      = "ubuntu-20-04-x64"
+  region     = "nyc3"
+  size       = "s-1vcpu-1gb"
   monitoring = true
   ssh_keys = [
     "98:9f:0d:4f:10:dc:61:a5:c5:14:f1:5a:88:cf:c1:76",
