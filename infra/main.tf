f3f8f74b6b866eec0af3910f4c31944e6f62cbbb
Riley Snyder <rileysnyder@protonmail.ch>
2021-12-21 15:40:52 -0600

    use DO for dns (#10)

--- infra/main.tf
+++ infra/main.tf
@@ -1,1 +1,38 @@
+resource "digitalocean_domain" "rileysnyder_org" {
+  name = "rileysnyder.org"
+}
 
+resource "digitalocean_record" "lighthouse" {
+  domain = digitalocean_domain.rileysnyder_org.name
+  type   = "A"
+  name   = "lighthouse"
+  value  = var.oc0
+}
+
+resource "digitalocean_record" "metrics" {
+  domain = digitalocean_domain.rileysnyder_org.name
+  type   = "A"
+  name   = "metrics"
+  value  = var.oc0
+}
+
+resource "digitalocean_record" "minecraft" {
+  domain = digitalocean_domain.rileysnyder_org.name
+  type   = "A"
+  name   = "minecraft"
+  value  = var.oc0
+}
+
+resource "digitalocean_record" "files" {
+  domain = digitalocean_domain.rileysnyder_org.name
+  type   = "A"
+  name   = "files"
+  value  = var.oc0
+}
+
+resource "digitalocean_record" "code" {
+  domain = digitalocean_domain.rileysnyder_org.name
+  type   = "A"
+  name   = "code"
+  value  = var.oc0
+}
---
+++ infra/var.tf
@@ -0,0 +1,49 @@
+variable "oc0" {
+  type    = string
+  default = "129.146.252.4"
+}
+
+variable "oc1" {
+  type    = string
+  default = "158.101.7.17"
+}
+
+variable "oc2" {
+  type    = string
+  default = "193.122.160.213"
+}
+
+variable "oc7" {
+  type    = string
+  default = "150.230.42.166"
+}
+
+variable "oc8" {
+  type    = string
+  default = "150.230.191.25"
+}
+
+variable "oc9" {
+  type    = string
+  default = "129.159.119.14"
+}
+
+variable "wubby" {
+  type    = string
+  default = "152.70.138.241"
+}
+
+variable "oca7" {
+  type    = string
+  default = "150.230.38.249"
+}
+
+variable "oca8" {
+  type    = string
+  default = "132.145.172.1"
+}
+
+variable "oca9" {
+  type    = string
+  default = "132.145.220.39"
+}
