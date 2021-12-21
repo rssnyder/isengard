cb95802b5da8a4f8d22fce1d559717fb5e8c0035
Riley Snyder <rileysndr@gmail.com>
2021-12-21 15:47:07 -0600

    add more domains

--- infra/main.tf
+++ infra/main.tf
@@ -36,3 +36,31 @@ resource "digitalocean_record" "code" {
   name   = "code"
   value  = var.oc0
 }
+
+resource "digitalocean_record" "qbittorrent" {
+  domain = digitalocean_domain.rileysnyder_org.name
+  type   = "A"
+  name   = "qbittorrent"
+  value  = var.oc0
+}
+
+resource "digitalocean_record" "public" {
+  domain = digitalocean_domain.rileysnyder_org.name
+  type   = "A"
+  name   = "public"
+  value  = var.oc0
+}
+
+resource "digitalocean_record" "wubby" {
+  domain = digitalocean_domain.rileysnyder_org.name
+  type   = "A"
+  name   = "wubby"
+  value  = var.wubby
+}
+
+resource "digitalocean_record" "tracker" {
+  domain = digitalocean_domain.rileysnyder_org.name
+  type   = "A"
+  name   = "tracker"
+  value  = var.wubby
+}
