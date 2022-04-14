7f07a91254cc666d037086370b643c5058070c76
Riley Snyder <rileysndr@gmail.com>
2022-04-14 07:47:39 -0500

    Update instances.tf

--- infra/instances.tf
+++ infra/instances.tf
@@ -58,26 +58,6 @@ resource "digitalocean_record" "do2" {
   value  = digitalocean_droplet.do2.ipv4_address
 }
 
-resource "digitalocean_droplet" "do2" {
-  name = "do2"
-
-  image      = "ubuntu-20-04-x64"
-  region     = "nyc3"
-  size       = "s-1vcpu-1gb"
-  monitoring = true
-  ssh_keys = [
-    "98:9f:0d:4f:10:dc:61:a5:c5:14:f1:5a:88:cf:c1:76",
-    "98:36:f6:72:ca:4e:5a:68:e6:3e:a1:aa:9f:76:61:73"
-  ]
-}
-
-resource "digitalocean_record" "do2" {
-  domain = digitalocean_domain.rileysnyder_org.name
-  type   = "A"
-  name   = digitalocean_droplet.do2.name
-  value  = digitalocean_droplet.do2.ipv4_address
-}
-
 resource "digitalocean_droplet" "do3" {
   name = "do3"
 
