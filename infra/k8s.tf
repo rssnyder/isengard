b63d5638a60b05e1541c4a38fc8d2e5c261d085e
Riley Snyder <rileysndr@gmail.com>
2022-08-02 18:15:56 -0500

    feat: add k8s cluster (#13)
    * feat: add k8s cluster
    
    * fmt

---
+++ infra/k8s.tf
@@ -0,0 +1,22 @@
+data "digitalocean_kubernetes_versions" "latest" {
+  version_prefix = "1.23."
+}
+
+resource "digitalocean_kubernetes_cluster" "ssenrah" {
+  name   = "ssenrah"
+  region = "nyc1"
+
+  auto_upgrade = true
+  version      = data.digitalocean_kubernetes_versions.latest.latest_version
+
+  maintenance_policy {
+    start_time = "04:00"
+    day        = "sunday"
+  }
+
+  node_pool {
+    name       = "core"
+    size       = "s-1vcpu-1gb"
+    node_count = 3
+  }
+}
