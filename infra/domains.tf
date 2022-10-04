6098a19765c27a54f8e734f4e58929bce521c5dc
Riley Snyder <rileysndr@gmail.com>
2022-10-03 21:38:36 -0500

    pihole terraform (#14)
    * pihole terraform
    
    * fmt
    
    * PIHOLE_PASSWORD
    
    * local
    
    * plan
    
    * global env
    
    * tf on kate
    
    * self-hosted k8s runners
    
    * setup-node
    
    * all self-hosted

--- infra/domains.tf
+++ infra/domains.tf
@@ -18,12 +18,23 @@ resource "digitalocean_record" "metrics" {
   value  = var.instances["hurley"].ip
 }
 
+resource "pihole_dns_record" "metrics" {
+  domain = "metrics.${digitalocean_domain.rileysnyder_org.name}"
+  ip     = var.instances["hurley_local"].ip
+}
+
 resource "digitalocean_record" "s3" {
   domain = digitalocean_domain.rileysnyder_org.name
   type   = "A"
   name   = "s3"
   value  = var.instances["hurley"].ip
 }
+
+resource "pihole_dns_record" "s3" {
+  domain = "s3.${digitalocean_domain.rileysnyder_org.name}"
+  ip     = var.instances["hurley_local"].ip
+}
+
 
 resource "digitalocean_record" "pushmetrics" {
   domain = digitalocean_domain.rileysnyder_org.name
@@ -46,11 +57,21 @@ resource "digitalocean_record" "files" {
   value  = var.instances["hurley"].ip
 }
 
+resource "pihole_dns_record" "files" {
+  domain = "files.${digitalocean_domain.rileysnyder_org.name}"
+  ip     = var.instances["hurley_local"].ip
+}
+
 resource "digitalocean_record" "requests" {
   domain = digitalocean_domain.rileysnyder_org.name
   type   = "A"
   name   = "requests"
   value  = var.instances["hurley"].ip
+}
+
+resource "pihole_dns_record" "requests" {
+  domain = "requests.${digitalocean_domain.rileysnyder_org.name}"
+  ip     = var.instances["hurley_local"].ip
 }
 
 resource "digitalocean_record" "parson_tesla" {
@@ -67,11 +88,21 @@ resource "digitalocean_record" "send" {
   value  = var.instances["hurley"].ip
 }
 
+resource "pihole_dns_record" "send" {
+  domain = "send.${digitalocean_domain.rileysnyder_org.name}"
+  ip     = var.instances["hurley_local"].ip
+}
+
 resource "digitalocean_record" "cameras" {
   domain = digitalocean_domain.rileysnyder_org.name
   type   = "A"
   name   = "cameras"
   value  = var.instances["hurley"].ip
+}
+
+resource "pihole_dns_record" "cameras" {
+  domain = "monitor.${digitalocean_domain.rileysnyder_org.name}"
+  ip     = var.instances["hurley_local"].ip
 }
 
 resource "digitalocean_record" "github_pages" {
@@ -103,4 +134,14 @@ module "babychelladj" {
   domain    = "rileysnyder.org"
   subdomain = "babychelladj"
   url       = "https://music.youtube.com/playlist?list=PL1d05GXUOoNnYBJPtv0h2LemZFDIKqaD4&jct=rsofholv4Gp4DpqXTY1kEuTRUQYKow"
+}
+
+resource "pihole_dns_record" "kate" {
+  domain = "kate.${digitalocean_domain.rileysnyder_org.name}"
+  ip     = var.instances["kate"].ip
+}
+
+resource "pihole_dns_record" "hurley" {
+  domain = "hurley.${digitalocean_domain.rileysnyder_org.name}"
+  ip     = var.instances["hurley_local"].ip
 }
--- infra/var.tf
+++ infra/var.tf
@@ -2,11 +2,20 @@ variable "instances" {
   description = "Map of instances"
   type        = map(any)
   default = {
+    kate = {
+      ip = "192.168.0.11"
+    }
     hurley = {
       ip = "173.22.88.59"
     }
+    hurley_local = {
+      ip = "192.168.0.3"
+    }
     oca0 = {
       ip = "129.146.241.139"
+    }
+    oca1 = {
+      ip = "129.153.88.119"
     }
   }
 }
