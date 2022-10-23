e49a69628af33ae294aaaf6ac717408cf5a97172
Riley Snyder <rileysnyder@protonmail.ch>
2022-10-23 11:08:31 -0500

    sync before trip

--- infra/provider.tf
+++ infra/provider.tf
@@ -7,6 +7,9 @@ terraform {
     pihole = {
       source = "ryanwholey/pihole"
     }
+    harness = {
+      source = "harness/harness"
+    }
   }
 }
 
@@ -14,4 +17,9 @@ provider "digitalocean" {}
 
 provider "pihole" {
   url = "http://192.168.0.3:8888"
+}
+
+provider "harness" {
+  endpoint   = "https://app.harness.io/gateway"
+  account_id = "-N_5zBuvRm2gzVAaNZ64lQ"
 }
