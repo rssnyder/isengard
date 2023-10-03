7463c79412e2b2d665340a28eeeb95b51f3e202e
Riley Snyder <rileysndr@gmail.com>
2023-10-03 13:56:20 +0100

    add help deployments

--- infra/k8s/helm.sh
+++ infra/k8s/helm.sh
@@ -2,6 +2,15 @@
 
 # helm installs in the cluster
 
-helm upgrade --install metallb metallb/metallb --namespace metallb-system --create-namespace
+helm upgrade -i metallb metallb/metallb --namespace metallb-system --create-namespace
 
-helm upgrade --install longhorn longhorn/longhorn --namespace longhorn-system --create-namespace --version 1.5.1
+helm upgrade -i longhorn longhorn/longhorn --namespace longhorn-system --create-namespace --version 1.5.1
+
+helm upgrade -i harness-delegate-work --namespace harness-delegate-work --create-namespace \
+  harness-delegate/harness-delegate-ng \
+  --set delegateName=lab \
+  --set accountId=wlgELJ0TTre5aZhzpt8gVA \
+  --set delegateToken=REDACTED \
+  --set managerEndpoint=https://app.harness.io/gratis \
+  --set delegateDockerImage=harness/delegate:23.09.80505 \
+  --set replicas=1 --set upgrader.enabled=true
---
+++ infra/k8s/k3sup.sh
@@ -0,0 +1,1 @@
+k3sup install --ip 192.168.0.57 --k3s-extra-args '--disable=servicelb' --user riley
