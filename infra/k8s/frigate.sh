b58515959873fcaedf6556b33a65755412688c62
Riley Snyder <rileysndr@gmail.com>
2023-10-20 23:10:25 +0100

    actions controller changes lots of other random stuff

--- infra/k8s/frigate.sh
+++ infra/k8s/frigate.sh
@@ -1,5 +1,3 @@
-kubectl apply -f frigate.yaml
-
 helm repo add blakeblackshear https://blakeblackshear.github.io/blakeshome-charts/
 
 helm upgrade --install frigate blakeblackshear/frigate --namespace frigate -f values/frigate.yaml
