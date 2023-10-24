4f2d665bf9e56796649484ce1da6b39e1516d5a0
Riley Snyder <rileysndr@gmail.com>
2023-10-24 16:47:07 +0100

    ingressClassName

--- infra/k8s/frigate.sh
+++
@@ -1,3 +0,0 @@
-helm repo add blakeblackshear https://blakeblackshear.github.io/blakeshome-charts/
-
-helm upgrade --install frigate blakeblackshear/frigate --namespace frigate -f values/frigate.yaml
--- infra/k8s/k3sup.sh
+++ infra/k8s/k3sup.sh
@@ -1,1 +1,1 @@
-k3sup install --ip 192.168.0.57 --k3s-extra-args '--disable=servicelb' --user riley
+k3sup install --ip 192.168.0.54 --k3s-extra-args '--disable=servicelb,traefik' --user riley
