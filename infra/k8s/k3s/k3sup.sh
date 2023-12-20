9531559a5d969d017dffe67a9b3865d3fed98311
Riley Snyder <rileysndr@gmail.com>
2023-12-20 17:57:42 -0600

    frigate resources new bot

--- infra/k8s/k3s/k3sup.sh
+++ infra/k8s/k3s/k3sup.sh
@@ -3,3 +3,6 @@ k3sup install --ip 192.168.0.10 --k3s-extra-args '--disable=servicelb,traefik --
 
 # ripper: worker
 k3sup join --ip 192.168.0.226 --server-ip 192.168.0.10 --user riley
+
+# t480: worker
+k3sup join --ip 192.168.0.4 --server-ip 192.168.0.10 --user riley
