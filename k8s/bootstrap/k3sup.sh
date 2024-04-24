606ed4966c7e4b2a483a5af27ff1cc6604555d36
Riley Snyder <rileysndr@gmail.com>
2024-04-23 19:53:20 -0500

    fix frigate config; move missing cameras

--- k8s/bootstrap/k3sup.sh
+++ k8s/bootstrap/k3sup.sh
@@ -1,14 +1,11 @@
 # charlie: master
 k3sup install --ip 192.168.0.10 --k3s-extra-args '--disable=servicelb,traefik' --user riley --k3s-version v1.28.5+k3s1
 
-# ripper: worker
-k3sup join --ip 192.168.0.226 --server-ip 192.168.0.10 --user riley
-
-# t480: worker
-k3sup join --ip 192.168.0.4 --server-ip 192.168.0.10 --user riley --k3s-version v1.28.5+k3s1
-
 # jack: worker
 k3sup join --ip 192.168.0.11 --server-ip 192.168.0.10 --user riley --k3s-version v1.28.5+k3s1
+
+# kate: worker
+k3sup join --ip 192.168.0.12 --server-ip 192.168.0.10 --user riley --k3s-version v1.28.5+k3s1
 
 
 kubectl drain <node-name> --ignore-daemonsets --delete-local-data
