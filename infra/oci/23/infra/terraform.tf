e117059e6ff3e1da34e5ea5bab167c6de0519b94
Riley Snyder <rileysnyder@protonmail.ch>
2025-05-06 13:50:37 -0500

    fix oci provider init

--- infra/oci/23/infra/terraform.tf
+++ infra/oci/23/infra/terraform.tf
@@ -20,3 +20,18 @@ terraform {
     }
   }
 }
+
+variable "tenancy_ocid" {}
+variable "user_ocid" {}
+variable "region" {}
+variable "fingerprint" {}
+variable "private_key_path" {}
+variable "compartment_ocid" {}
+
+provider "oci" {
+   region = var.region
+   tenancy_ocid = var.tenancy_ocid
+   user_ocid = var.user_ocid
+   fingerprint = var.fingerprint
+   private_key_path = var.private_key_path
+}
